629a1cfafeb1fda167d1825db4f38b7fad1f44b9
Denylson Melo <denylsonmelo@gmail.com>
2016-08-29 20:00:00 -0300

    adicionado codigo de Avaliador feito anteriormente

--- src/main/java/br/edu/ifpi/capar/leilao/negocio/Avaliador.java
+++ src/main/java/br/edu/ifpi/capar/leilao/negocio/Avaliador.java
@@ -7,28 +7,70 @@ package br.edu.ifpi.capar.leilao.negocio;
 
 import br.edu.ifpi.capar.leilao.modelo.Lance;
 import br.edu.ifpi.capar.leilao.modelo.Leilao;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 /**
  *
  * @author Denylson Melo
  */
 public class Avaliador {
-    
+
     private double maiorLance = Double.NEGATIVE_INFINITY;
     private double menorLance = Double.POSITIVE_INFINITY;
-    
-    public void avaliar(Leilao leilao){
-        if(leilao.getLances().isEmpty())
+    private double media;
+    private List<Lance> tresMaiores;
+
+    public void avaliar(Leilao leilao) {
+        if (leilao.getLances().isEmpty()) {
             throw new IllegalArgumentException("Não posso avaliar leilão sem lances");
-        
+        }
+
+        media = 0;
         for (Lance lance : leilao.getLances()) {
-            if (lance.getValor()>maiorLance) {
+            if (lance.getValor() > maiorLance) {
                 maiorLance = lance.getValor();
             }
-            if(lance.getValor()<menorLance){
+            if (lance.getValor() < menorLance) {
                 menorLance = lance.getValor();
             }
+            media = media + lance.getValor();
         }
+        media = media / leilao.getLances().size();
+
+        tresMaiores = new ArrayList<>(leilao.getLances());
+        Collections.sort(tresMaiores, new Comparator<Lance>() {
+
+            public int compare(Lance lance1, Lance lance2) {
+                if (lance1.getValor() < lance2.getValor()) {
+                    return 1;
+                }
+                if (lance1.getValor() > lance2.getValor()) {
+                    return -1;
+                }
+                return 0;
+            }
+        });
+
+        int tamanhoDaLista = 0;
+        if (tresMaiores.size() > 3) {
+            tamanhoDaLista = 3;
+
+        } else {
+            tamanhoDaLista = tresMaiores.size();
+        }
+
+        tresMaiores = tresMaiores.subList(0, tamanhoDaLista);
+    }
+
+    public List<Lance> getTresMaiores() {
+        return tresMaiores;
+    }
+
+    public double getMedia() {
+        return media;
     }
 
     public double getMaiorLance() {
--- src/test/java/br/edu/ifpi/capar/leilao/negocio/AvaliadorTest.java
+++ src/test/java/br/edu/ifpi/capar/leilao/negocio/AvaliadorTest.java
@@ -8,7 +8,6 @@ package br.edu.ifpi.capar.leilao.negocio;
 import br.edu.ifpi.capar.leilao.modelo.Lance;
 import br.edu.ifpi.capar.leilao.modelo.Leilao;
 import br.edu.ifpi.capar.leilao.modelo.Usuario;
-import br.edu.ifpi.capar.leilao.negocio.Avaliador;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -17,37 +16,141 @@ import org.junit.Test;
  * @author Denylson Melo
  */
 public class AvaliadorTest {
-    
+
     @Test
-    public void verificaMaiorLanceEMenorLance() {
+    public void deveAvaliarLeilaoEmOrdemCrescente() {
         //cenario
         Usuario usuario1 = new Usuario("denylson");
-        Usuario usuario2 = new Usuario("safadao");
-        
+        Usuario usuario2 = new Usuario("melo");
+
         Lance lance1 = new Lance(usuario1, 500.0);
         Lance lance2 = new Lance(usuario2, 600.0);
         Lance lance3 = new Lance(usuario1, 700.0);
-        
+
         Leilao leilao = new Leilao("leilao de ps4");
         leilao.darLance(lance1);
         leilao.darLance(lance2);
         leilao.darLance(lance3);
-        
+
         Avaliador avaliador = new Avaliador();
-        
+
         //acao
         avaliador.avaliar(leilao);
-        
+
         //validacao
         Assert.assertEquals(700, avaliador.getMaiorLance(), 0.0000001);
         Assert.assertEquals(500, avaliador.getMenorLance(), 0.0000001);
-                
+        Assert.assertEquals(600, avaliador.getMedia(), 0.000001);
     }
-    
-    @Test(expected = IllegalArgumentException.class)
-    public void naoDeveAvaliarLeilaoSemLances(){
-        Leilao leilao = new Leilao("leilao para alguma coisa");
+
+    @Test
+    public void deveAvaliarLeilaoEmOrdemDecrescente() {
+        //cenario
+        Usuario usuario1 = new Usuario("denylson");
+        Usuario usuario2 = new Usuario("melo");
+
+        Lance lance1 = new Lance(usuario1, 700.0);
+        Lance lance2 = new Lance(usuario2, 600.0);
+        Lance lance3 = new Lance(usuario1, 500.0);
+
+        Leilao leilao = new Leilao("leilao de ps4");
+        leilao.darLance(lance1);
+        leilao.darLance(lance2);
+        leilao.darLance(lance3);
+
         Avaliador avaliador = new Avaliador();
+
+        //acao
+        avaliador.avaliar(leilao);
+
+        //validacao
+        Assert.assertEquals(700, avaliador.getMaiorLance(), 0.0000001);
+        Assert.assertEquals(500, avaliador.getMenorLance(), 0.0000001);
+        Assert.assertEquals(600, avaliador.getMedia(), 0.000001);
+    }
+
+    @Test
+    public void deveAvaliarLeilaoEmOrdemRandomica() {
+        //cenario
+        Usuario usuario1 = new Usuario("denylson");
+        Usuario usuario2 = new Usuario("melo");
+
+        Lance lance1 = new Lance(usuario1, 600.0);
+        Lance lance2 = new Lance(usuario2, 500.0);
+        Lance lance3 = new Lance(usuario1, 700.0);
+
+        Leilao leilao = new Leilao("leilao de ps4");
+        leilao.darLance(lance1);
+        leilao.darLance(lance2);
+        leilao.darLance(lance3);
+
+        Avaliador avaliador = new Avaliador();
+
+        //acao
+        avaliador.avaliar(leilao);
+
+        //validacao
+        Assert.assertEquals(700, avaliador.getMaiorLance(), 0.0000001);
+        Assert.assertEquals(500, avaliador.getMenorLance(), 0.0000001);
+        Assert.assertEquals(600, avaliador.getMedia(), 0.000001);
+    }
+
+    @Test
+    public void deveAvaliarLeilaoDeUmLanceSo() {
+        //cenario
+        Usuario usuario = new Usuario("denylson");
+
+        Lance lance1 = new Lance(usuario, 500.0);
+
+        Leilao leilao = new Leilao("leilao de ps4");
+        leilao.darLance(lance1);
+
+        Avaliador avaliador = new Avaliador();
+
+        //acao
+        avaliador.avaliar(leilao);
+
+        //validacao
+        Assert.assertEquals(500, avaliador.getMaiorLance(), 0.0000001);
+        Assert.assertEquals(500, avaliador.getMenorLance(), 0.0000001);
+        Assert.assertEquals(500, avaliador.getMedia(), 0.000001);
+    }
+
+    @Test
+    public void deveRetornar3MaioresLances() {
+        //cenario
+        Usuario usuario1 = new Usuario("denylson");
+        Usuario usuario2 = new Usuario("melo");
+
+        Lance lance1 = new Lance(usuario1, 600.0);
+        Lance lance2 = new Lance(usuario2, 500.0);
+        Lance lance3 = new Lance(usuario1, 700.0);
+        Lance lance4 = new Lance(usuario2, 800.0);
+        Lance lance5 = new Lance(usuario1, 900.0);
+
+        Leilao leilao = new Leilao("leilao de ps4");
+        leilao.darLance(lance1);
+        leilao.darLance(lance2);
+        leilao.darLance(lance3);
+        leilao.darLance(lance4);
+        leilao.darLance(lance5);
+
+        Avaliador avaliador = new Avaliador();
+
+        //acao
+        avaliador.avaliar(leilao);
+
+        //validacao
+        Assert.assertEquals(3, avaliador.getTresMaiores().size());
+        Assert.assertEquals(900, avaliador.getTresMaiores().get(0).getValor(), 0.00001);
+        Assert.assertEquals(800, avaliador.getTresMaiores().get(1).getValor(), 0.00001);
+        Assert.assertEquals(700, avaliador.getTresMaiores().get(2).getValor(), 0.00001);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void naoDeveAvaliarLeilaoSemLances() {
+        Avaliador avaliador = new Avaliador();
+        Leilao leilao = new Leilao("leilao para alguma coisa");
         avaliador.avaliar(leilao);
     }
 }
