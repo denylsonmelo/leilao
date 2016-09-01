c3a8a141c17c8d8d0d8ee84d67fcee0ed9c80e8e
FonteneleLucas <lucasfind@hotmail.com>
2016-09-01 17:18:40 -0300

    Commit AvaliadorBuilder, Filtro_OK

--- src/test/java/br/edu/ifpi/capar/leilao/desafio/FiltroTest.java
+++ src/test/java/br/edu/ifpi/capar/leilao/desafio/FiltroTest.java
@@ -5,10 +5,11 @@
  */
 package br.edu.ifpi.capar.leilao.desafio;
 
-import br.edu.ifpi.capar.leilao.modelo.Lance;
-import br.edu.ifpi.capar.leilao.modelo.Leilao;
+import br.edu.ifpi.capar.leilao.builder.LeilaoBuilder;
 import br.edu.ifpi.capar.leilao.modelo.Usuario;
-import org.junit.Assert;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -16,31 +17,116 @@ import org.junit.Test;
  * @author Denylson Melo
  */
 public class FiltroTest {
+
+    private Usuario user01;
+    private Usuario user02;
+    private Filtro filtro;
+    private LeilaoBuilder builderDeLeilao;
+
+    @Before
+    public void setup() {
+        user01 = new Usuario("Usuario 01");
+        user02 = new Usuario("Usuario 02");
+        filtro = new Filtro();
+        builderDeLeilao = new LeilaoBuilder();
+        builderDeLeilao = new LeilaoBuilder().para("Leilao para alguma coisa!!");
+        
+    }
+
+    @Test
+    public void deveSelecionarLancesEntre1000E3000() {
+        //Cenario
+        builderDeLeilao.darLance(user01, 2000)
+                .darLance(user02, 3000)
+                .darLance(user01, 2500)
+                .darLance(user02, 2999)
+                .darLance(user01, 2001);
+        
+        //Acão
+        filtro.filtrarLeilao(builderDeLeilao.build());
+        
+        //Validação
+        assertThat(filtro.getLancesFiltrados().size(), equalTo(4));
+        assertThat(filtro.getLancesFiltrados().get(0).getValor(), equalTo(2000.0));
+        assertThat(filtro.getLancesFiltrados().get(1).getValor(), equalTo(2500.0));
+        assertThat(filtro.getLancesFiltrados().get(2).getValor(), equalTo(2999.0));
+        assertThat(filtro.getLancesFiltrados().get(3).getValor(), equalTo(2001.0));
+    }
     
     @Test
-    public void deveSelecionarLancesEntre1000E3000(){
-        Usuario usuario1 = new Usuario("fulano");
-        Usuario usuario2 = new Usuario("beltrano");
+    public void selecionarLancesEntre500E700() {
+        //Cenario
+        builderDeLeilao.darLance(user01, 680)
+                       .darLance(user02, 699)
+                       .darLance(user01, 550)
+                       .darLance(user02, 600)
+                       .darLance(user01, 5000)
+                       .darLance(user02, 100);
+
+        //Acão
+        filtro.filtrarLeilao(builderDeLeilao.build());
         
-        Lance lance1 = new Lance(usuario1, 2000);
-        Lance lance2 = new Lance(usuario2, 3000);
-        Lance lance3 = new Lance(usuario1, 2500);
-        Lance lance4 = new Lance(usuario2, 2999);
-        Lance lance5 = new Lance(usuario1, 10000);
-        
-        Leilao leilao = new Leilao("leilao para alguma coisa");
-        leilao.darLance(lance1);
-        leilao.darLance(lance2);
-        leilao.darLance(lance3);
-        leilao.darLance(lance4);
-        leilao.darLance(lance5);
-        
-        Filtro filtro = new Filtro();
-        filtro.filtrarLeilao(leilao);
-        
-        Assert.assertEquals(3, filtro.getLancesFiltrados().size());
-        Assert.assertEquals(2000, filtro.getLancesFiltrados().get(0).getValor(), 0.0000001);
-        Assert.assertEquals(2500, filtro.getLancesFiltrados().get(1).getValor(), 0.0000001);
-        Assert.assertEquals(2999, filtro.getLancesFiltrados().get(2).getValor(), 0.0000001);
+        //Validação
+        assertThat(filtro.getLancesFiltrados().size(), equalTo(4));
+        assertThat(filtro.getLancesFiltrados().get(0).getValor(), equalTo(680.0));
+        assertThat(filtro.getLancesFiltrados().get(1).getValor(), equalTo(699.0));
+        assertThat(filtro.getLancesFiltrados().get(2).getValor(), equalTo(550.0));
+        assertThat(filtro.getLancesFiltrados().get(3).getValor(), equalTo(600.0));   
     }
+    
+    @Test
+    public void selecionarLancesMaioresQue5000() {
+        //Cenario
+        builderDeLeilao.darLance(user01, 400)
+                       .darLance(user02, 5001)
+                       .darLance(user01, 10000)
+                       .darLance(user02, 200)
+                       .darLance(user01, 7000);
+
+        //Acão
+        filtro.filtrarLeilao(builderDeLeilao.build());
+        
+        //Validação
+        assertThat(filtro.getLancesFiltrados().size(), equalTo(3));
+        assertThat(filtro.getLancesFiltrados().get(0).getValor(), equalTo(5001.0));
+        assertThat(filtro.getLancesFiltrados().get(1).getValor(), equalTo(10000.0));
+        assertThat(filtro.getLancesFiltrados().get(2).getValor(), equalTo(7000.0));  
+    }
+    
+    @Test
+    public void eliminarMenoresQue500() {
+        //Cenario
+        builderDeLeilao.darLance(user01, 100)
+                       .darLance(user02, 201)
+                       .darLance(user01, 300)
+                       .darLance(user02, 5500)
+                       .darLance(user01, 7000);
+
+        //Acão
+        filtro.filtrarLeilao(builderDeLeilao.build());
+        
+        //Validação
+        assertThat(filtro.getLancesFiltrados().size(), equalTo(2));
+        assertThat(filtro.getLancesFiltrados().get(0).getValor(), equalTo(5500.0));
+        assertThat(filtro.getLancesFiltrados().get(1).getValor(), equalTo(7000.0));
+    }
+    
+    @Test
+    public void eliminarValoresEntre3000E5000() {
+        //Cenario
+        builderDeLeilao.darLance(user01, 3001)
+                       .darLance(user02, 4000)
+                       .darLance(user01, 5000)
+                       .darLance(user02, 100)
+                       .darLance(user01, 600);
+
+        //Acão
+        filtro.filtrarLeilao(builderDeLeilao.build());
+        
+        //Validação
+        assertThat(filtro.getLancesFiltrados().size(), equalTo(1));
+        assertThat(filtro.getLancesFiltrados().get(0).getValor(), equalTo(600.0)); 
+    }
+    
+    
 }
--- src/test/java/br/edu/ifpi/capar/leilao/negocio/AvaliadorTest.java
+++ src/test/java/br/edu/ifpi/capar/leilao/negocio/AvaliadorTest.java
@@ -5,10 +5,11 @@
  */
 package br.edu.ifpi.capar.leilao.negocio;
 
-import br.edu.ifpi.capar.leilao.modelo.Lance;
-import br.edu.ifpi.capar.leilao.modelo.Leilao;
+import br.edu.ifpi.capar.leilao.builder.LeilaoBuilder;
 import br.edu.ifpi.capar.leilao.modelo.Usuario;
-import org.junit.Assert;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -17,140 +18,103 @@ import org.junit.Test;
  */
 public class AvaliadorTest {
 
+    private Usuario user01;
+    private Usuario user02;
+    private Avaliador avaliador;
+    private LeilaoBuilder builderDeLeilao;
+
+    @Before
+    public void setup() {
+        user01 = new Usuario("Usuario 01");
+        user02 = new Usuario("Usuario 02");
+        avaliador = new Avaliador();
+        builderDeLeilao = new LeilaoBuilder().para("Leilao de algo!!");
+    }
+
     @Test
     public void deveAvaliarLeilaoEmOrdemCrescente() {
         //cenario
-        Usuario usuario1 = new Usuario("denylson");
-        Usuario usuario2 = new Usuario("melo");
-
-        Lance lance1 = new Lance(usuario1, 500.0);
-        Lance lance2 = new Lance(usuario2, 600.0);
-        Lance lance3 = new Lance(usuario1, 700.0);
-
-        Leilao leilao = new Leilao("leilao de ps4");
-        leilao.darLance(lance1);
-        leilao.darLance(lance2);
-        leilao.darLance(lance3);
-
-        Avaliador avaliador = new Avaliador();
-
+        builderDeLeilao.darLance(user01, 500)
+                .darLance(user02, 600)
+                .darLance(user01, 700);
+        
         //acao
-        avaliador.avaliar(leilao);
+        avaliador.avaliar(builderDeLeilao.build());
 
         //validacao
-        Assert.assertEquals(700, avaliador.getMaiorLance(), 0.0000001);
-        Assert.assertEquals(500, avaliador.getMenorLance(), 0.0000001);
-        Assert.assertEquals(600, avaliador.getMedia(), 0.000001);
+        assertThat(avaliador.getMaiorLance(), equalTo(700.0));
+        assertThat(avaliador.getMenorLance(), equalTo(500.0));
+        assertThat(avaliador.getMedia(), equalTo(600.0));
     }
 
     @Test
     public void deveAvaliarLeilaoEmOrdemDecrescente() {
         //cenario
-        Usuario usuario1 = new Usuario("denylson");
-        Usuario usuario2 = new Usuario("melo");
-
-        Lance lance1 = new Lance(usuario1, 700.0);
-        Lance lance2 = new Lance(usuario2, 600.0);
-        Lance lance3 = new Lance(usuario1, 500.0);
-
-        Leilao leilao = new Leilao("leilao de ps4");
-        leilao.darLance(lance1);
-        leilao.darLance(lance2);
-        leilao.darLance(lance3);
-
-        Avaliador avaliador = new Avaliador();
-
+        builderDeLeilao.darLance(user01, 700)
+                       .darLance(user02, 600)
+                       .darLance(user01, 500);
         //acao
-        avaliador.avaliar(leilao);
+        avaliador.avaliar(builderDeLeilao.build());
 
         //validacao
-        Assert.assertEquals(700, avaliador.getMaiorLance(), 0.0000001);
-        Assert.assertEquals(500, avaliador.getMenorLance(), 0.0000001);
-        Assert.assertEquals(600, avaliador.getMedia(), 0.000001);
+        assertThat(avaliador.getMaiorLance(), equalTo(700.0));
+        assertThat(avaliador.getMenorLance(), equalTo(500.0));
+        assertThat(avaliador.getMedia(), equalTo(600.0));
+
     }
 
     @Test
     public void deveAvaliarLeilaoEmOrdemRandomica() {
         //cenario
-        Usuario usuario1 = new Usuario("denylson");
-        Usuario usuario2 = new Usuario("melo");
-
-        Lance lance1 = new Lance(usuario1, 600.0);
-        Lance lance2 = new Lance(usuario2, 500.0);
-        Lance lance3 = new Lance(usuario1, 700.0);
-
-        Leilao leilao = new Leilao("leilao de ps4");
-        leilao.darLance(lance1);
-        leilao.darLance(lance2);
-        leilao.darLance(lance3);
-
-        Avaliador avaliador = new Avaliador();
-
+        builderDeLeilao.darLance(user01, 600)
+                       .darLance(user02,500)
+                       .darLance(user01, 700);
         //acao
-        avaliador.avaliar(leilao);
+        avaliador.avaliar(builderDeLeilao.build());
 
         //validacao
-        Assert.assertEquals(700, avaliador.getMaiorLance(), 0.0000001);
-        Assert.assertEquals(500, avaliador.getMenorLance(), 0.0000001);
-        Assert.assertEquals(600, avaliador.getMedia(), 0.000001);
+        assertThat(avaliador.getMaiorLance(), equalTo(700.0));
+        assertThat(avaliador.getMenorLance(), equalTo(500.0));
+        assertThat(avaliador.getMedia(), equalTo(600.0));
+        
     }
 
     @Test
     public void deveAvaliarLeilaoDeUmLanceSo() {
         //cenario
-        Usuario usuario = new Usuario("denylson");
-
-        Lance lance1 = new Lance(usuario, 500.0);
-
-        Leilao leilao = new Leilao("leilao de ps4");
-        leilao.darLance(lance1);
-
-        Avaliador avaliador = new Avaliador();
-
+        builderDeLeilao.darLance(user01, 500);
+        
         //acao
-        avaliador.avaliar(leilao);
+        avaliador.avaliar(builderDeLeilao.build());
 
         //validacao
-        Assert.assertEquals(500, avaliador.getMaiorLance(), 0.0000001);
-        Assert.assertEquals(500, avaliador.getMenorLance(), 0.0000001);
-        Assert.assertEquals(500, avaliador.getMedia(), 0.000001);
+        assertThat(avaliador.getMaiorLance(), equalTo(500.0));
+        assertThat(avaliador.getMenorLance(), equalTo(500.0));
+        assertThat(avaliador.getMedia(), equalTo(500.0));
     }
 
     @Test
     public void deveRetornar3MaioresLances() {
         //cenario
-        Usuario usuario1 = new Usuario("denylson");
-        Usuario usuario2 = new Usuario("melo");
-
-        Lance lance1 = new Lance(usuario1, 600.0);
-        Lance lance2 = new Lance(usuario2, 500.0);
-        Lance lance3 = new Lance(usuario1, 700.0);
-        Lance lance4 = new Lance(usuario2, 800.0);
-        Lance lance5 = new Lance(usuario1, 900.0);
-
-        Leilao leilao = new Leilao("leilao de ps4");
-        leilao.darLance(lance1);
-        leilao.darLance(lance2);
-        leilao.darLance(lance3);
-        leilao.darLance(lance4);
-        leilao.darLance(lance5);
-
-        Avaliador avaliador = new Avaliador();
-
+        builderDeLeilao.darLance(user01, 600)
+                .darLance(user02, 500)
+                .darLance(user01, 700)
+                .darLance(user02, 800)
+                .darLance(user01, 900);
+        
         //acao
-        avaliador.avaliar(leilao);
+        avaliador.avaliar(builderDeLeilao.build());
 
         //validacao
-        Assert.assertEquals(3, avaliador.getTresMaiores().size());
-        Assert.assertEquals(900, avaliador.getTresMaiores().get(0).getValor(), 0.00001);
-        Assert.assertEquals(800, avaliador.getTresMaiores().get(1).getValor(), 0.00001);
-        Assert.assertEquals(700, avaliador.getTresMaiores().get(2).getValor(), 0.00001);
+        assertThat(avaliador.getTresMaiores().size(), equalTo(3));
+        assertThat(avaliador.getTresMaiores().get(0).getValor(), equalTo(900.0));
+        assertThat(avaliador.getTresMaiores().get(1).getValor(), equalTo(800.0));
+        assertThat(avaliador.getTresMaiores().get(2).getValor(), equalTo(700.0));
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void naoDeveAvaliarLeilaoSemLances() {
-        Avaliador avaliador = new Avaliador();
-        Leilao leilao = new Leilao("leilao para alguma coisa");
-        avaliador.avaliar(leilao);
+        avaliador.avaliar(builderDeLeilao.build());
+        
     }
 }
