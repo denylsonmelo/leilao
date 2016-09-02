ff10852680407f435379481d487b230397a4d739
Denylson Melo <denylsonmelo@gmail.com>
2016-09-02 02:52:54 -0300

    removendo mencao aos templates do netbeans

--- src/test/java/br/edu/ifpi/capar/leilao/desafio/PalindromoTest.java
+++ src/test/java/br/edu/ifpi/capar/leilao/desafio/PalindromoTest.java
@@ -1,11 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.edu.ifpi.capar.leilao.desafio;
 
-import br.edu.ifpi.capar.leilao.desafio.Palindromo;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -14,21 +8,21 @@ import org.junit.Test;
  * @author Denylson Melo
  */
 public class PalindromoTest {
-    
+
     @Test
-    public void deveReconhecerPalindromoSemHifen(){
+    public void deveReconhecerPalindromoSemHifen() {
         String string = "Anotaram a data da maratona";
         Assert.assertTrue(Palindromo.isPalindromo(string));
     }
-    
+
     @Test
-    public void deveReconhecerPalindromoComHifen(){
+    public void deveReconhecerPalindromoComHifen() {
         String string = "Socorram-me subi no onibus em Marrocos";
         Assert.assertTrue(Palindromo.isPalindromo(string));
     }
-    
+
     @Test
-    public void naoDeveReconhecerPalindromo(){
+    public void naoDeveReconhecerPalindromo() {
         String string = "Alguma frase bolada aqui";
         Assert.assertFalse(Palindromo.isPalindromo(string));
     }
