1746cb75b8d10bc8ef4a49d3dc0f33b7df418916
Denylson Melo <denylsonmelo@gmail.com>
2016-09-02 16:34:04 -0300

    adicionando matcher ainda não funcionando

---
+++ src/test/java/br/edu/ifpi/capar/leilao/matcher/LeilaoMatcher.java
@@ -0,0 +1,34 @@
+package br.edu.ifpi.capar.leilao.matcher;
+
+import br.edu.ifpi.capar.leilao.modelo.Lance;
+import br.edu.ifpi.capar.leilao.modelo.Leilao;
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+
+/**
+ *
+ * @author 2155060
+ */
+public class LeilaoMatcher extends TypeSafeMatcher {
+
+    private final Lance lance;
+
+    public LeilaoMatcher(Lance lance) {
+        this.lance = lance;
+    }
+
+    @Override
+    protected boolean matchesSafely(Object t) {
+        return true; //t.getLances().contains(lance);
+    }
+
+    @Override
+    public void describeTo(Description d) {
+        d.appendText("leilao tem um lance com valor igual a " + lance.getValor());
+    }
+
+    public static Matcher<Leilao> temUmLance(Lance lance) {
+        return new LeilaoMatcher(lance);
+    }
+}
